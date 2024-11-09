db6ac2e77516ef04953e2424f8deffed25dc3899
ayoubjebb2001 <jebbouri1968@gmail.com>
2024-11-09 15:36:05 +0100

    challenge9

---
+++ challenge9.js
@@ -0,0 +1,6 @@
+function addnames(firstName, lastName) {
+    console.log(firstName+" "+lastName);
+}
+
+addnames("John", "Doe")     // Should return "John Doe"
+addnames("Mary", "Smith")   // Should return "Mary Smith"
